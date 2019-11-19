c8730030d13f8301a2f68d79569bb0df99fd94a3
kcomain <cheunghowui@gmail.com>
2019-11-19 23:07:57 +0800

    v0.2.5

--- cogs/fun.py
+++ cogs/fun.py
@@ -17,5 +17,32 @@ class Fun(commands.Cog):
         embed.set_footer(text='WHAT A BAD GUY >:((((((((')
         await ctx.send(embed=embed)
 
+    @commands.command(name='pyramid')
+    async def pyramid(self,ctx,height:int):
+        '''Make a pyramid.
+        '''
+        count = 1
+        string = ''
+        temp = []
+        for i in range(0,height):
+            for k in range(0,count):
+                string += '*'
+            count += 2
+            temp.append(string)
+            string = ''
+        baselen = len([a for a in temp[len(temp)-1]])
+        spaces = int(baselen / 2)
+        for i in range(0,height):
+            temp3 = ''
+            for k in range(0,spaces):
+                temp3 += ' '
+            temp[i] = temp3 + temp[i]
+            spaces -= 1
+        string = ''
+        for i in temp:
+            string += i
+            string += '\n'
+        await ctx.send(embed=discord.Embed(title='Pyramid Size={}'.format(height),description='```{}```'.format(string),colour=0x00FF00))
+
 def setup(bot):
     bot.add_cog(Fun(bot))
