7203c65071b8ecd54379db22c0eec75e7aa21c60
Kenny <cheunghowui@gmail.com>
2020-01-22 01:55:48 +0000

    Added developer check

--- cogs/util/checks.py
+++ cogs/util/checks.py
@@ -1,4 +1,9 @@
 import discord
 from discord.ext import commands
 
-def has_mute()
+class checks():
+    @staticmethod
+    def is_dev():
+        async def predicate(ctx):
+            return ctx.author.id in [397029587965575170,348921660361146380]
+        return commands.check(predicate)
